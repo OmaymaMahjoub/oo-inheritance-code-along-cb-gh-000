b0d0427e147e1813abacd989b1ccda149ceb6fde
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-16 15:34:51 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,18 @@
 class Vehicle
 
+  def initialize(size,number)
+    @wheel_size=size
+    @wheel_number=number
+  end
+
+  attr_accessor :wheel_number, :wheel_size
+
+  def go
+    return 'vrrrrrrrooom!'
+  end
+
+  def fill_up_tank
+    return 'filling up!'
+  end
+
 end
